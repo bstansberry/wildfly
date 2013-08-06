heiko-braun/jboss-as

[WFLY-490] Wiring in a configurable access control system Add scoped role configuration

domain-management/src/main/java/org/jboss/as/domain/management/access/HostScopedRolesResourceDefinition.java
@@ -1,0 +1,112 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.domain.management.access;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.HOST;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.HOST_SCOPED_ROLE;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.jboss.as.controller.AttributeDefinition;
+import org.jboss.as.controller.AttributeMarshaller;
+import org.jboss.as.controller.ListAttributeDefinition;
+import org.jboss.as.controller.OperationDefinition;
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.ReloadRequiredWriteAttributeHandler;
+import org.jboss.as.controller.SimpleAttributeDefinition;
+import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
+import org.jboss.as.controller.SimpleListAttributeDefinition;
+import org.jboss.as.controller.SimpleOperationDefinitionBuilder;
+import org.jboss.as.controller.SimpleResourceDefinition;
+import org.jboss.as.controller.access.ConfigurableAuthorizer;
+import org.jboss.as.controller.access.constraint.HostEffectConstraint;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
+import org.jboss.as.controller.parsing.Attribute;
+import org.jboss.as.controller.parsing.Element;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.as.domain.management._private.DomainManagementResolver;
+import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.ModelType;
+
+/**
+ * {@link org.jboss.as.controller.ResourceDefinition} for an administrative role that is
+ * scoped to a particular set of hosts.
+ *
+ * @author Brian Stansberry (c) 2013 Red Hat Inc.
+ */
+public class HostScopedRolesResourceDefinition  extends SimpleResourceDefinition {
+
+    public static final PathElement PATH_ELEMENT = PathElement.pathElement(HOST_SCOPED_ROLE);
+
+    public static final SimpleAttributeDefinition BASE_ROLE =
+            new SimpleAttributeDefinitionBuilder(ModelDescriptionConstants.BASE_ROLE, ModelType.STRING)
+            .setRestartAllServices()
+            .build();
+
+
+    public static final ListAttributeDefinition HOSTS = SimpleListAttributeDefinition.Builder.of(ModelDescriptionConstants.HOSTS,
+                new SimpleAttributeDefinitionBuilder(HOST, ModelType.STRING)
+                        .setAttributeMarshaller(new AttributeMarshaller() {
+                            @Override
+                            public void marshallAsElement(AttributeDefinition attribute, ModelNode resourceModel, boolean marshallDefault, XMLStreamWriter writer) throws XMLStreamException {
+                                writer.writeEmptyElement(Element.SERVER_GROUP.getLocalName());
+                                writer.writeAttribute(Attribute.NAME.getLocalName(), resourceModel.asString());
+                            }
+                        }).build())
+            .setMinSize(1)
+            .setWrapXmlList(false)
+            .build();
+
+    private final HostScopedRoleAdd addHandler;
+    private final HostScopedRoleRemove removeHandler;
+    private final HostScopedRoleWriteAttributeHandler writeAttributeHandler;
+
+    public HostScopedRolesResourceDefinition(ConfigurableAuthorizer authorizer) {
+        super(PATH_ELEMENT, DomainManagementResolver.getResolver("core.access-control.host-scoped-role"));
+
+        Map<String, HostEffectConstraint> constraintMap = new HashMap<String, HostEffectConstraint>();
+        this.addHandler = new HostScopedRoleAdd(constraintMap, authorizer);
+        this.removeHandler =  new HostScopedRoleRemove(constraintMap, authorizer);
+        this.writeAttributeHandler = new HostScopedRoleWriteAttributeHandler(constraintMap);
+    }
+
+    @Override
+    public void registerOperations(ManagementResourceRegistration resourceRegistration) {
+        registerAddOperation(resourceRegistration, addHandler);
+        OperationDefinition removeDef = new SimpleOperationDefinitionBuilder(ModelDescriptionConstants.REMOVE, getResourceDescriptionResolver())
+                .build();
+        resourceRegistration.registerOperationHandler(removeDef, removeHandler);
+    }
+
+    @Override
+    public void registerAttributes(ManagementResourceRegistration resourceRegistration) {
+        super.registerAttributes(resourceRegistration);
+
+        resourceRegistration.registerReadWriteAttribute(BASE_ROLE, null, new ReloadRequiredWriteAttributeHandler(BASE_ROLE));
+        resourceRegistration.registerReadWriteAttribute(HOSTS, null, writeAttributeHandler);
+    }
+}

host-controller/src/main/java/org/jboss/as/host/controller/parsing/DomainXml.java
@@ -24,8 +24,10 @@ package org.jboss.as.host.controller.parsing;
 
 import static javax.xml.stream.XMLStreamConstants.END_ELEMENT;
 import static org.jboss.as.controller.ControllerMessages.MESSAGES;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ACCESS_CONTROL;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CONTENT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CORE_SERVICE;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DEPLOYMENT;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DEPLOYMENT_OVERLAY;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.EXTENSION;
@@ -88,6 +90,7 @@ import org.jboss.as.controller.persistence.SubsystemMarshallingContext;
 import org.jboss.as.controller.resource.SocketBindingGroupResourceDefinition;
 import org.jboss.as.domain.controller.resources.DomainRootDefinition;
 import org.jboss.as.domain.controller.resources.ServerGroupResourceDefinition;
+import org.jboss.as.domain.management.parsing.ManagementXml;
 import org.jboss.as.server.controller.resources.DeploymentAttributes;
 import org.jboss.as.server.parsing.CommonXml;
 import org.jboss.dmr.ModelNode;
@@ -132,11 +135,14 @@ public class DomainXml extends CommonXml {
             case DOMAIN_1_3:
                 readDomainElement1_3(reader, new ModelNode(), readerNS, nodes);
                 break;
+            case DOMAIN_1_4:
+                readDomainElement1_4(reader, new ModelNode(), readerNS, nodes);
+                break;
             default:
                 // Instead of having to list the remaining versions we just check it is actually a valid version.
                 for (Namespace current : Namespace.domainValues()) {
                     if (readerNS.equals(current)) {
-                        readDomainElement1_4(reader, new ModelNode(), readerNS, nodes);
+                        readDomainElement2_0(reader, new ModelNode(), readerNS, nodes);
                         return;
                     }
                 }
@@ -171,6 +177,15 @@ public class DomainXml extends CommonXml {
             writePaths(writer, modelNode.get(PATH), true);
             writeNewLine(writer);
         }
+
+        if (modelNode.hasDefined(CORE_SERVICE) && modelNode.get(CORE_SERVICE).hasDefined(ACCESS_CONTROL)) {
+
+            ManagementXml managementXml = new ManagementXml(new ManagementXmlDelegate());
+            //TODO pass in proper access-constraint node
+            managementXml.writeManagement(writer, new ModelNode(), modelNode.get(CORE_SERVICE, ACCESS_CONTROL), true);
+            writeNewLine(writer);
+        }
+
         if (modelNode.hasDefined(PROFILE)) {
             writer.writeStartElement(Element.PROFILES.getLocalName());
             for (final Property profile : modelNode.get(PROFILE).asPropertyList()) {
@@ -402,6 +417,71 @@ public class DomainXml extends CommonXml {
         }
         if (element == Element.PATHS) {
             parsePaths(reader, address, expectedNs, list, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.PROFILES) {
+            parseProfiles(reader, address, expectedNs, list);
+            element = nextElement(reader, expectedNs);
+        }
+        final Set<String> interfaceNames = new HashSet<String>();
+        if (element == Element.INTERFACES) {
+            parseInterfaces(reader, interfaceNames, address, expectedNs, list, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.SOCKET_BINDING_GROUPS) {
+            parseDomainSocketBindingGroups(reader, address, expectedNs, list, interfaceNames);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.DEPLOYMENTS) {
+            parseDeployments(reader, address, expectedNs, list, EnumSet.of(Attribute.NAME, Attribute.RUNTIME_NAME),
+                    EnumSet.of(Element.CONTENT, Element.FS_ARCHIVE, Element.FS_EXPLODED));
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.DEPLOYMENT_OVERLAYS) {
+            parseDeploymentOverlays(reader, expectedNs, new ModelNode(), list, true, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.SERVER_GROUPS) {
+            parseServerGroups(reader, address, expectedNs, list);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.MANAGEMENT_CLIENT_CONTENT) {
+            parseManagementClientContent(reader, address, expectedNs, list);
+            element = nextElement(reader, expectedNs);
+        } else if (element == null) {
+            // Always add op(s) to set up management-client-content resources
+            initializeRolloutPlans(address, list);
+        } else {
+            throw unexpectedElement(reader);
+        }
+    }
+
+    void readDomainElement2_0(final XMLExtendedStreamReader reader, final ModelNode address, final Namespace expectedNs, final List<ModelNode> list) throws XMLStreamException {
+
+        parseNamespaces(reader, address, list);
+
+        // attributes
+        readDomainElementAttributes_1_3(reader, expectedNs, address, list);
+
+        // Content
+        // Handle elements: sequence
+
+        Element element = nextElement(reader, expectedNs);
+        if (element == Element.EXTENSIONS) {
+            extensionXml.parseExtensions(reader, address, expectedNs, list);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.SYSTEM_PROPERTIES) {
+            parseSystemProperties(reader, address, expectedNs, list, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.PATHS) {
+            parsePaths(reader, address, expectedNs, list, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.MANAGEMENT) {
+            ManagementXml managementXml = new ManagementXml(new ManagementXmlDelegate());
+            managementXml.parseManagement(reader, address, expectedNs, list, false);
             element = nextElement(reader, expectedNs);
         }
         if (element == Element.PROFILES) {
@@ -1034,5 +1114,47 @@ public class DomainXml extends CommonXml {
             }
             writer.writeEndElement();
         }
+    }
+
+    private class ManagementXmlDelegate extends ManagementXml.Delegate {
+
+        @Override
+        public void parseSecurityRealms(XMLExtendedStreamReader reader, ModelNode address, Namespace expectedNs, List<ModelNode> list) throws XMLStreamException {
+            // Not supported yet
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void parseOutboundConnections(XMLExtendedStreamReader reader, ModelNode address, Namespace expectedNs, List<ModelNode> list) throws XMLStreamException {
+            // Not supported yet
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void parseAccessControl(final XMLExtendedStreamReader reader, final ModelNode address, final Namespace expectedNs,
+                                       final List<ModelNode> list) throws XMLStreamException {
+            ParseUtils.requireNoAttributes(reader);
+
+            while (reader.hasNext() && reader.nextTag() != END_ELEMENT) {
+                requireNamespace(reader, expectedNs);
+                final Element element = Element.forName(reader.getLocalName());
+                switch (element) {
+                    case SERVER_GROUP_SCOPED_ROLES:
+                        ManagementXml.parseServerGroupScopedRoles(reader, address, expectedNs, list);
+                        break;
+                    case HOST_SCOPED_ROLES:
+                        ManagementXml.parseHostScopedRoles(reader, address, expectedNs, list);
+                        break;
+                    case CONSTRAINTS: {
+                        ManagementXml.parseAccessControlConstraints(reader, address, expectedNs, list);
+                        break;
+                    }
+                    default: {
+                        throw unexpectedElement(reader);
+                    }
+                }
+            }
+        }
+
     }
 }

bstansberry/wildfly

domain-management/src/main/java/org/jboss/as/domain/management/access/HostScopedRolesResourceDefinition.java
@@ -1,0 +1,112 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.domain.management.access;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.HOST;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.HOST_SCOPED_ROLE;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.jboss.as.controller.AttributeDefinition;
+import org.jboss.as.controller.AttributeMarshaller;
+import org.jboss.as.controller.ListAttributeDefinition;
+import org.jboss.as.controller.OperationDefinition;
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.ReloadRequiredWriteAttributeHandler;
+import org.jboss.as.controller.SimpleAttributeDefinition;
+import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
+import org.jboss.as.controller.SimpleListAttributeDefinition;
+import org.jboss.as.controller.SimpleOperationDefinitionBuilder;
+import org.jboss.as.controller.SimpleResourceDefinition;
+import org.jboss.as.controller.access.ConfigurableAuthorizer;
+import org.jboss.as.controller.access.constraint.HostEffectConstraint;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
+import org.jboss.as.controller.parsing.Attribute;
+import org.jboss.as.controller.parsing.Element;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.as.domain.management._private.DomainManagementResolver;
+import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.ModelType;
+
+/**
+ * {@link org.jboss.as.controller.ResourceDefinition} for an administrative role that is
+ * scoped to a particular set of hosts.
+ *
+ * @author Brian Stansberry (c) 2013 Red Hat Inc.
+ */
+public class HostScopedRolesResourceDefinition extends SimpleResourceDefinition {
+
+    public static final PathElement PATH_ELEMENT = PathElement.pathElement(HOST_SCOPED_ROLE);
+
+    public static final SimpleAttributeDefinition BASE_ROLE =
+            new SimpleAttributeDefinitionBuilder(ModelDescriptionConstants.BASE_ROLE, ModelType.STRING)
+            .setRestartAllServices()
+            .build();
+
+
+    public static final ListAttributeDefinition HOSTS = SimpleListAttributeDefinition.Builder.of(ModelDescriptionConstants.HOSTS,
+                new SimpleAttributeDefinitionBuilder(HOST, ModelType.STRING)
+                        .setAttributeMarshaller(new AttributeMarshaller() {
+                            @Override
+                            public void marshallAsElement(AttributeDefinition attribute, ModelNode resourceModel, boolean marshallDefault, XMLStreamWriter writer) throws XMLStreamException {
+                                writer.writeEmptyElement(Element.HOST.getLocalName());
+                                writer.writeAttribute(Attribute.NAME.getLocalName(), resourceModel.asString());
+                            }
+                        }).build())
+            .setAllowNull(true)
+            .setWrapXmlList(false)
+            .build();
+
+    private final HostScopedRoleAdd addHandler;
+    private final HostScopedRoleRemove removeHandler;
+    private final HostScopedRoleWriteAttributeHandler writeAttributeHandler;
+
+    public HostScopedRolesResourceDefinition(ConfigurableAuthorizer authorizer) {
+        super(PATH_ELEMENT, DomainManagementResolver.getResolver("core.access-control.host-scoped-role"));
+
+        Map<String, HostEffectConstraint> constraintMap = new HashMap<String, HostEffectConstraint>();
+        this.addHandler = new HostScopedRoleAdd(constraintMap, authorizer);
+        this.removeHandler =  new HostScopedRoleRemove(constraintMap, authorizer);
+        this.writeAttributeHandler = new HostScopedRoleWriteAttributeHandler(constraintMap);
+    }
+
+    @Override
+    public void registerOperations(ManagementResourceRegistration resourceRegistration) {
+        registerAddOperation(resourceRegistration, addHandler);
+        OperationDefinition removeDef = new SimpleOperationDefinitionBuilder(ModelDescriptionConstants.REMOVE, getResourceDescriptionResolver())
+                .build();
+        resourceRegistration.registerOperationHandler(removeDef, removeHandler);
+    }
+
+    @Override
+    public void registerAttributes(ManagementResourceRegistration resourceRegistration) {
+        super.registerAttributes(resourceRegistration);
+
+        resourceRegistration.registerReadWriteAttribute(BASE_ROLE, null, new ReloadRequiredWriteAttributeHandler(BASE_ROLE));
+        resourceRegistration.registerReadWriteAttribute(HOSTS, null, writeAttributeHandler);
+    }
+}

host-controller/src/main/java/org/jboss/as/host/controller/parsing/DomainXml.java
@@ -24,8 +24,10 @@ package org.jboss.as.host.controller.parsing;
 
 import static javax.xml.stream.XMLStreamConstants.END_ELEMENT;
 import static org.jboss.as.controller.ControllerMessages.MESSAGES;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ACCESS_CONTROL;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CONTENT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CORE_SERVICE;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DEPLOYMENT;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DEPLOYMENT_OVERLAY;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.EXTENSION;
@@ -88,6 +90,7 @@ import org.jboss.as.controller.persistence.SubsystemMarshallingContext;
 import org.jboss.as.controller.resource.SocketBindingGroupResourceDefinition;
 import org.jboss.as.domain.controller.resources.DomainRootDefinition;
 import org.jboss.as.domain.controller.resources.ServerGroupResourceDefinition;
+import org.jboss.as.domain.management.parsing.ManagementXml;
 import org.jboss.as.server.controller.resources.DeploymentAttributes;
 import org.jboss.as.server.parsing.CommonXml;
 import org.jboss.dmr.ModelNode;
@@ -132,11 +135,14 @@ public class DomainXml extends CommonXml {
             case DOMAIN_1_3:
                 readDomainElement1_3(reader, new ModelNode(), readerNS, nodes);
                 break;
+            case DOMAIN_1_4:
+                readDomainElement1_4(reader, new ModelNode(), readerNS, nodes);
+                break;
             default:
                 // Instead of having to list the remaining versions we just check it is actually a valid version.
                 for (Namespace current : Namespace.domainValues()) {
                     if (readerNS.equals(current)) {
-                        readDomainElement1_4(reader, new ModelNode(), readerNS, nodes);
+                        readDomainElement2_0(reader, new ModelNode(), readerNS, nodes);
                         return;
                     }
                 }
@@ -171,6 +177,15 @@ public class DomainXml extends CommonXml {
             writePaths(writer, modelNode.get(PATH), true);
             writeNewLine(writer);
         }
+
+        if (modelNode.hasDefined(CORE_SERVICE) && modelNode.get(CORE_SERVICE).hasDefined(ACCESS_CONTROL)) {
+
+            ManagementXml managementXml = new ManagementXml(new ManagementXmlDelegate());
+            //TODO pass in proper access-constraint node
+            managementXml.writeManagement(writer, new ModelNode(), modelNode.get(CORE_SERVICE, ACCESS_CONTROL), true);
+            writeNewLine(writer);
+        }
+
         if (modelNode.hasDefined(PROFILE)) {
             writer.writeStartElement(Element.PROFILES.getLocalName());
             for (final Property profile : modelNode.get(PROFILE).asPropertyList()) {
@@ -402,6 +417,71 @@ public class DomainXml extends CommonXml {
         }
         if (element == Element.PATHS) {
             parsePaths(reader, address, expectedNs, list, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.PROFILES) {
+            parseProfiles(reader, address, expectedNs, list);
+            element = nextElement(reader, expectedNs);
+        }
+        final Set<String> interfaceNames = new HashSet<String>();
+        if (element == Element.INTERFACES) {
+            parseInterfaces(reader, interfaceNames, address, expectedNs, list, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.SOCKET_BINDING_GROUPS) {
+            parseDomainSocketBindingGroups(reader, address, expectedNs, list, interfaceNames);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.DEPLOYMENTS) {
+            parseDeployments(reader, address, expectedNs, list, EnumSet.of(Attribute.NAME, Attribute.RUNTIME_NAME),
+                    EnumSet.of(Element.CONTENT, Element.FS_ARCHIVE, Element.FS_EXPLODED));
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.DEPLOYMENT_OVERLAYS) {
+            parseDeploymentOverlays(reader, expectedNs, new ModelNode(), list, true, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.SERVER_GROUPS) {
+            parseServerGroups(reader, address, expectedNs, list);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.MANAGEMENT_CLIENT_CONTENT) {
+            parseManagementClientContent(reader, address, expectedNs, list);
+            element = nextElement(reader, expectedNs);
+        } else if (element == null) {
+            // Always add op(s) to set up management-client-content resources
+            initializeRolloutPlans(address, list);
+        } else {
+            throw unexpectedElement(reader);
+        }
+    }
+
+    void readDomainElement2_0(final XMLExtendedStreamReader reader, final ModelNode address, final Namespace expectedNs, final List<ModelNode> list) throws XMLStreamException {
+
+        parseNamespaces(reader, address, list);
+
+        // attributes
+        readDomainElementAttributes_1_3(reader, expectedNs, address, list);
+
+        // Content
+        // Handle elements: sequence
+
+        Element element = nextElement(reader, expectedNs);
+        if (element == Element.EXTENSIONS) {
+            extensionXml.parseExtensions(reader, address, expectedNs, list);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.SYSTEM_PROPERTIES) {
+            parseSystemProperties(reader, address, expectedNs, list, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.PATHS) {
+            parsePaths(reader, address, expectedNs, list, false);
+            element = nextElement(reader, expectedNs);
+        }
+        if (element == Element.MANAGEMENT) {
+            ManagementXml managementXml = new ManagementXml(new ManagementXmlDelegate());
+            managementXml.parseManagement(reader, address, expectedNs, list, false);
             element = nextElement(reader, expectedNs);
         }
         if (element == Element.PROFILES) {
@@ -1034,5 +1114,47 @@ public class DomainXml extends CommonXml {
             }
             writer.writeEndElement();
         }
+    }
+
+    private class ManagementXmlDelegate extends ManagementXml.Delegate {
+
+        @Override
+        public void parseSecurityRealms(XMLExtendedStreamReader reader, ModelNode address, Namespace expectedNs, List<ModelNode> list) throws XMLStreamException {
+            // Not supported yet
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void parseOutboundConnections(XMLExtendedStreamReader reader, ModelNode address, Namespace expectedNs, List<ModelNode> list) throws XMLStreamException {
+            // Not supported yet
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void parseAccessControl(final XMLExtendedStreamReader reader, final ModelNode address, final Namespace expectedNs,
+                                       final List<ModelNode> list) throws XMLStreamException {
+            ParseUtils.requireNoAttributes(reader);
+            ModelNode accContAddr = address.clone().add(CORE_SERVICE, ACCESS_CONTROL);
+            while (reader.hasNext() && reader.nextTag() != END_ELEMENT) {
+                requireNamespace(reader, expectedNs);
+                final Element element = Element.forName(reader.getLocalName());
+                switch (element) {
+                    case SERVER_GROUP_SCOPED_ROLES:
+                        ManagementXml.parseServerGroupScopedRoles(reader, accContAddr, expectedNs, list);
+                        break;
+                    case HOST_SCOPED_ROLES:
+                        ManagementXml.parseHostScopedRoles(reader, accContAddr, expectedNs, list);
+                        break;
+                    case CONSTRAINTS: {
+                        ManagementXml.parseAccessControlConstraints(reader, address, expectedNs, list);
+                        break;
+                    }
+                    default: {
+                        throw unexpectedElement(reader);
+                    }
+                }
+            }
+        }
+
     }
 }