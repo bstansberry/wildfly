b9b08a6c39b45987020cc30b08298535c1946ede
Brian Stansberry <brian.stansberry@redhat.com>
2014-04-09 19:48:17 -0500

    [WFLY-3092] Get rid of useless ServerUpdateResultHandler

--- host-controller/src/main/java/org/jboss/as/domain/controller/plan/AbstractServerGroupRolloutTask.java
+++ host-controller/src/main/java/org/jboss/as/domain/controller/plan/AbstractServerGroupRolloutTask.java
@@ -22,9 +22,6 @@
 
 package org.jboss.as.domain.controller.plan;
 
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CANCELLED;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
-
 import org.jboss.as.controller.AccessAuditContext;
 import org.jboss.as.controller.remote.TransactionalProtocolClient;
 import org.jboss.as.domain.controller.DomainControllerLogger;
@@ -47,14 +44,12 @@ abstract class AbstractServerGroupRolloutTask implements Runnable {
     protected final List<ServerUpdateTask> tasks;
     protected final ServerUpdatePolicy updatePolicy;
     protected final ServerTaskExecutor executor;
-    protected final ServerUpdateTask.ServerUpdateResultHandler resultHandler;
     protected final Subject subject;
 
-    public AbstractServerGroupRolloutTask(List<ServerUpdateTask> tasks, ServerUpdatePolicy updatePolicy, ServerTaskExecutor executor, final ServerUpdateTask.ServerUpdateResultHandler resultHandler, Subject subject) {
+    public AbstractServerGroupRolloutTask(List<ServerUpdateTask> tasks, ServerUpdatePolicy updatePolicy, ServerTaskExecutor executor, Subject subject) {
         this.tasks = tasks;
         this.updatePolicy = updatePolicy;
         this.executor = executor;
-        this.resultHandler = resultHandler;
         this.subject = subject;
     }
 
@@ -92,13 +87,6 @@ abstract class AbstractServerGroupRolloutTask implements Runnable {
         // final ModelNode transformedResult = prepared.getOperation().transformResult(preparedResult);
         updatePolicy.recordServerResult(identity, preparedResult);
         executor.recordPreparedOperation(prepared);
-        resultHandler.handleServerUpdateResult(identity, preparedResult);
-    }
-
-    protected void sendCancelledResponse(ServerIdentity serverId) {
-        final ModelNode response = new ModelNode();
-        response.get(OUTCOME).set(CANCELLED);
-        resultHandler.handleServerUpdateResult(serverId, response);
     }
 
 }
