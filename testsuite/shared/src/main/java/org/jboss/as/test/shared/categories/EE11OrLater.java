4c310bbf10a1734ba74ec518b9e1a4a60286fcf1
Brian Stansberry <brian.stansberry@redhat.com>
2024-03-29 15:32:42 -0500

    [WFLY-19185] Allow exclusion of tests needing EE10 using a JUnit Category

---
+++ testsuite/shared/src/main/java/org/jboss/as/test/shared/categories/EE11OrLater.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.jboss.as.test.shared.categories;
+
+/**
+ * Category for tests that require EE 11 or later.
+ */
+public interface EE11OrLater {
+}
