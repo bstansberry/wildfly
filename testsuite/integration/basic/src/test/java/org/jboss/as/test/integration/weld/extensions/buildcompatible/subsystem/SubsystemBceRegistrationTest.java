a1fdb1b64bafe3fb407db9af29344aea1d6af302
Matej Novotny <manovotn@redhat.com>
2024-08-20 17:15:25 +0200

    Move the test module setup into ServerSetupTask

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/extensions/buildcompatible/subsystem/SubsystemBceRegistrationTest.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/extensions/buildcompatible/subsystem/SubsystemBceRegistrationTest.java
@@ -3,11 +3,13 @@ package org.jboss.as.test.integration.weld.extensions.buildcompatible.subsystem;
 import jakarta.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.as.test.module.util.TestModule;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,31 +18,11 @@ import java.io.File;
 import java.net.URL;
 
 @RunWith(Arquillian.class)
-    public class SubsystemBceRegistrationTest {
-
-    private static final String MODULE_NAME = "build-compatible-extension";
-    private static TestModule testModule;
-
-    public static void doSetup() throws Exception {
-        URL url = BCEExtension.class.getResource(MODULE_NAME + "-module.xml");
-        File moduleXmlFile = new File(url.toURI());
-        testModule = new TestModule("test." + MODULE_NAME, moduleXmlFile);
-        testModule.addResource("bce-extension.jar")
-                .addClasses(BCEExtension.class, RegisteredExtension.class, RegisteredBean.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
-                .addAsServiceProvider(org.jboss.as.controller.Extension.class, BCEExtension.class);
-        testModule.create();
-
-    }
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        testModule.remove();
-    }
+@ServerSetup(SubsystemBceRegistrationTest.SetupTask.class)
+public class SubsystemBceRegistrationTest {
 
     @Deployment
     public static WebArchive getDeployment() throws Exception {
-        doSetup();
         return ShrinkWrap.create(WebArchive.class)
                 .addClasses(SubsystemBceRegistrationTest.class, DummyBean.class, TestModule.class, RegisteredBean.class)
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
@@ -59,5 +41,27 @@ import java.net.URL;
 
         // verify BCE was executed; if so, RegisteredBean would now be resolvable
         Assert.assertNotNull(registeredBean);
+    }
+
+    public static class SetupTask implements ServerSetupTask {
+        private static final String MODULE_NAME = "build-compatible-extension";
+        private static TestModule testModule;
+
+        @Override
+        public void setup(ManagementClient managementClient, String containerId) throws Exception {
+            URL url = BCEExtension.class.getResource(MODULE_NAME + "-module.xml");
+            File moduleXmlFile = new File(url.toURI());
+            testModule = new TestModule("test." + MODULE_NAME, moduleXmlFile);
+            testModule.addResource("bce-extension.jar")
+                    .addClasses(BCEExtension.class, RegisteredExtension.class, RegisteredBean.class)
+                    .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                    .addAsServiceProvider(org.jboss.as.controller.Extension.class, BCEExtension.class);
+            testModule.create();
+        }
+
+        @Override
+        public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
+            testModule.remove();
+        }
     }
 }
